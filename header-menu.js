34dcbdeadd1f1fb102b43f97ee95c278fb4461bf
spendy-github <158532470+spendy-github@users.noreply.github.com>
2024-02-25 08:07:06 +0300

    mobilka menushka

--- header-menu.js
+++ header-menu.js
@@ -1,4 +1,5 @@
 document.querySelector('.header__menu').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.header__list').classList.toggle('open');
+    document.querySelector('.header__background').classList.toggle('open');
 })
